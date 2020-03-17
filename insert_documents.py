436875e5b55ccf7dc88a008e1af0ce43e58de731
Student 227951 <227951@student.pwr.edu.pl>
2020-03-17 22:43:44 +0100

    added sh scripts for bulk insert and clearing index; write docs to one file works

--- insert_documents.py
+++ insert_documents.py
@@ -1,8 +1,6 @@
 import sys
-import json
 import glob
 from pathlib import Path    
-from pprint import pprint
 from elasticsearch import Elasticsearch
 
 root_dir = sys.argv[1]
@@ -11,10 +9,8 @@ elastic_client = Elasticsearch(['localhost'], port=9200)
 
 for path in glob.iglob(root_dir +'/**', recursive=True):
     filename = Path(path).name
-    print('checking if should index: ' + filename)
     if (filename.startswith('blogs') or filename.startswith('news')) and filename.endswith('.json'):
         try:
-            print('indexing: ' + path)
             file = open(path, 'r').read()
             elastic_client.index(index='news_articles', doc_type='Blog', body=file)
         except:
--- write_documents_into_one.py
+++ write_documents_into_one.py
@@ -1,21 +1,25 @@
 import sys
-import os
-import json
 import glob
 from pathlib import Path
-from pprint import pprint
 
 root_dir = sys.argv[1]
 result_file_path = sys.argv[2]
 
+num_files = int(sys.argv[3])
+num_indexed = 1
+
+
 for path in glob.iglob(root_dir + '/**', recursive=True):
+    if num_indexed > num_files:
+        break
     filename = Path(path).name
     print('checking if should index: ' + filename)
-    if filename.startswith('blogs') and filename.endswith('.json'):
+    if (filename.startswith('blogs') or filename.startswith('news')) and filename.endswith('.json'):
         with open(result_file_path, "a") as myfile:
             try:
                 file = open(path, 'r').read()
                 myfile.writelines('{ "index" : { "_index" : "news_articles"} }\n')
                 myfile.writelines(file + '\n')
+                num_indexed = num_indexed + 1
             except:
                 print("Error when reading " + filename)
